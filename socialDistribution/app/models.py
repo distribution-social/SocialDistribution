4f63498dbe2a057821669e5d896486e155c90f0e
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2023-02-08 11:45:45 -0700

    Add comments

--- socialDistribution/app/models.py
+++ socialDistribution/app/models.py
@@ -5,19 +5,20 @@ from django.db import models
 
 
 class Author(models.Model):
+    # Unique ID of the user
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
 
+    # Server host of the user
     host = models.URLField(max_length=200)
 
+    # Max Length: 35 (First Name) + 35 (Last Name)
     displayName = models.CharField(max_length=70)
 
     github = models.URLField(max_length=200, unique=True)
 
     profileImage = models.URLField(max_length=200, unique=True)
 
-    # Max Length: 64 (Username) + 255 (Domain) 
+    # Max Length: 64 (Username) + 255 (Domain)
     email = models.EmailField(max_length=320, unique=True)
 
     username = models.CharField(max_length=30, unique=True)
-
-    
