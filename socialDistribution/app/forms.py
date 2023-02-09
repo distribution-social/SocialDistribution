eed27468e26184588211c81a00d9497e310e267f
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2023-02-08 17:21:53 -0700

    Implemented signup

--- socialDistribution/app/forms.py
+++ socialDistribution/app/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 class SignupForm(forms.Form):
     display_name = forms.CharField(label='Display Name', max_length=50, required=True)
+    username = forms.CharField(
+        label='Username', max_length=50, required=True)
     email = forms.EmailField(label='Email', required=True)
     github_url = forms.CharField(label='Github url', required=False)
     password = forms.CharField(
--- socialDistribution/app/views.py
+++ socialDistribution/app/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
 from .forms import SignupForm
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def signup(request):
-    context = {"title": "signup", "form": SignupForm()}
-    return render(request, 'signup.html', context)
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            
+            display_name = form.cleaned_data.get('display_name')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            github_url = form.cleaned_data.get('github_url')
+            password = form.cleaned_data.get('password')
+            confirm_password = form.cleaned_data.get('confirm_password')
+
+            user = User.objects.create_user(
+                username, email, password)
+            
+            user.save()
+
+            user = authenticate(username=username, password=password)
+            
+            login(request, user)
+            # Redirect to home page
+            # print(display_name, username, email, github_url, password)
+
+        # print(request.POST.get("display_name"))
+        return HttpResponse("Successfully signed up!")
+    elif request.method == "GET":
+        context = {"title": "signup", "form": SignupForm()}
+        return render(request, 'signup.html', context)
