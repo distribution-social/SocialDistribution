4b1e77b927155207aaffcb9b554c47cda5f60408
NickSNFK <nwielgus@hotmail.com>
2023-03-03 11:52:24 -0700

    fix inbox usernames to use ids

--- socialDistribution/app/templatetags/extra_tags.py
+++ socialDistribution/app/templatetags/extra_tags.py
@@ -9,14 +9,14 @@ def classname(obj):
   return obj.__class__.__name__
 
 @register.filter(name='isRequest')
-def is_request(obj,username):
-  user = Author.objects.get(username=obj.username)
-  return len(user.sent_requests.filter(username=username)) >= 1
+def is_request(user_id,id):
+  user = Author.objects.get(id=user_id)
+  return len(user.sent_requests.filter(id=id)) >= 1
 
 @register.filter(name='isFollowing')
-def is_following(obj,username):
-  user = Author.objects.get(username=obj.username)
-  return len(user.following.filter(username=username)) >= 1
+def is_following(user_id,id):
+  user = Author.objects.get(id=user_id)
+  return len(user.following.filter(id=id)) >= 1
 
 @register.filter
 def convert_username_to_id(value):
