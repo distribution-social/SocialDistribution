e2cf350fcac5b7e8af8ba75c845d275517d67ecd
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2023-03-20 21:36:19 -0600

    Heroku Integration

--- socialDistribution/socialDistribution/settings.py
+++ socialDistribution/socialDistribution/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import sys
-
-import sys
+import os
+import django_on_heroku  # top of the file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,6 +149,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
@@ -156,3 +158,5 @@ REST_FRAMEWORK = {
 
     ]
 }
+
+django_on_heroku.settings(locals())  # bottom of the file
