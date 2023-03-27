07105fba2022c290d35e8192e1c0f66f1b57a051
“Ronggang <ronggang999@gmail.com>
2023-03-26 21:09:36 -0600

    model and button

--- socialDistribution/app/models.py
+++ socialDistribution/app/models.py
@@ -149,10 +149,11 @@ class Post(models.Model):
     ]
     visibility = models.CharField(max_length=7, choices=VISIBILITY_CHOICES)
     unlisted = models.BooleanField(default=False)
-    image = models.CharField(max_length=100,null=True, blank=True)
+    image = models.ImageField(upload_to='images/')
 
     likes = GenericRelation(Like)
     activity = GenericRelation(Activity)
+    
 
     def save(self, *args, **kwargs):
         if self.origin == '' or not self.origin:
