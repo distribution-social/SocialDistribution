735fcacb191936f05516457fc2b495d7497971aa
“Ronggang <ronggang999@gmail.com>
2023-04-01 14:23:14 -0600

    hot fix

--- socialDistribution/app/template_views.py
+++ socialDistribution/app/template_views.py
@@ -2,16 +2,6 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 import json
 from .forms import CommentForm
-from django import template
-register = template.Library()
-from django.utils.safestring import mark_safe
-import markdown
-
-
-@register.filter
-def markdownify(value):
-    md = markdown.Markdown(extensions=['markdown.extensions.fenced_code', 'markdown.extensions.tables', 'markdown.extensions.codehilite', 'markdown.extensions.toc', 'markdown.extensions.extra'])
-    return mark_safe(md.convert(value))
 
 
 def get_post_card_template(request):
