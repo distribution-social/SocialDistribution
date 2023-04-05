7c93b8cfb470dbbd461bd4a0415a1e9ca34627ce
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2023-04-05 15:54:40 -0600

    Add ALLOW_ALL_HEADERS

--- socialDistribution/socialDistribution/settings.py
+++ socialDistribution/socialDistribution/settings.py
@@ -58,7 +58,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = "access-control-allow-origin"
+CORS_ALLOW_ALL_HEADERS = True
 
 CORS_ALLOWED_ORIGINS = [
 "https://www.distribution.social",
